bb64736f709552771a2163ac9eaf25b0458d3afd
saketgiri <127030887+saketgiri@users.noreply.github.com>
2025-06-02 12:53:36 +0000

    with Math

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,3 +5,18 @@ const gameName = new String("NewGame");
 console.log(`My name is ${name} and my repo count is ${repoCount}`);
 console.log(`My name is ${gameName} and my game number us ${gameNumber}`);
 console.log(gameName.indexOf("e"))
+
+const num1 = 100;
+const num2 = new Number(100);
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(num1 === num2);
+
+let num3 = num2;
+num3 = 100;
+console.log(num1, num2, num3);
+
+
+//########################################################################
+console.log(Math)
