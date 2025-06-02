db2afd62ff63b9f9b4cf131d6449f709ebda7da5
saketgiri <127030887+saketgiri@users.noreply.github.com>
2025-06-02 04:29:41 +0000

    learn data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 144553;
+let accountEmail = "saketware@google.com";
+var accountPassword = "12345";
+let accountState;
+accountCity = "Blore";
+
+// accountId = 1;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+// alert( 3 + 3) // we are using Node.js not browser
+console.log(null == null);
+console.log(undefined == 0);
+console.log(undefined == undefined);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "saket");
+console.log(typeof (2 ** 100));
