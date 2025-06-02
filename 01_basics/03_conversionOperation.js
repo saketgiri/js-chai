0865510f653aa9d7d19cce50ab26f7de97423afa
saketgiri <127030887+saketgiri@users.noreply.github.com>
2025-06-02 05:21:37 +0000

    Conversion between datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let sample = true;
+// let valueInNumber = Number(sample);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// true to 1 and false to 0
+// null gets converted to 0 when cast a number
+// undefined gets cast to NaN
+
+// string that cannot be converted to numbers get converted to NaN 
+// NaN is a value of type number
+// Number is a constructor for number
+
+// let value = " ";
+// let valueAsBoolean = Boolean(value);
+// console.log(typeof valueAsBoolean);
+// console.log(valueAsBoolean);
+
+//empty string goes to false, non-empty goes to true
+//0 goes to false, all else goes to true
+
+let someNumber = 22;
+let numberAsString = String(someNumber);
+console.log(typeof numberAsString);
+console.log(numberAsString);
